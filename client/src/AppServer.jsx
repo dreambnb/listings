717921cdd2cf964d99a71cdaa284f43e3e446f8b
Dylan Qian <dylanqian@Dylans-MacBook-Pro.local>
2018-05-12 14:01:28 -0700

    finished webpack config for server side rendering

---
+++ client/src/AppServer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SimilarListings from './index.jsx';
+
+// ReactDOM.render(<SimilarListings currentListingId={50}/>, document.getElementById('similarListing'));
+
+export default SimilarListings;
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,107 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+const webpack = require('webpack');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
-module.exports = {
+const client = {
   entry: `${SRC_DIR}/App.jsx`,
   output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
+    filename: 'app.js',
+    path: DIST_DIR,
+  }
+};
+
+const server = {
+  entry: `${SRC_DIR}/AppServer.jsx`,
+  target: 'node',
+  output: {
+    path: DIST_DIR,
+    filename: 'app-server.js',
+    libraryTarget: 'commonjs-module'
   },
   module: {
     rules: [
-        {
-          test: /\.jsx?$/,
-          include: SRC_DIR,
-          loader: "babel-loader",
-          options: {
-              presets: ["es2015", "react"]
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'isomorphic-style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              // importLoaders: 1,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
           }
-      
+        ]
+      }
+      ],
+    }
+  }
+
+  const common = {
+    module: {
+      rules: [{
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader',
         },
         {
-          test: /\.css$/,
-          include: SRC_DIR,
-          use: [
-            {
-               loader: 'style-loader',
-            },
-            {
-               loader: 'css-loader',
-               options: {
-                  sourceMap: true,
-                  modules: true,
-                  localIdentName: '[local]___[hash:base64:5]'
-                 }
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]',
+          },
+        },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
             }
-            ],
-        }, 
-        {
-          test: /\.(png|jpg|gif)$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 8192
-              }
-            }
-          ]
-        }
-    ]
-}
+          }
+        ]
+      }
+    ],
+  }
 };
+
+module.exports = [
+  Object.assign({}, common, client),
+  Object.assign({}, server)
+];
