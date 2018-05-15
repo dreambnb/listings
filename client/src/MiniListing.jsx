d8bfc2cba8e9a77dfa3e1a2070d4723a5befd373
Dylan Qian <dylanqian@Dylans-MacBook-Pro.local>
2018-05-14 19:37:10 -0700

    add the dots for loading

--- client/src/MiniListing.jsx
+++ client/src/MiniListing.jsx
@@ -5,14 +5,14 @@ import styles from './MiniListingStyle.css';
 
 
 // comment below out for client side rendering
-const MiniListingCSS = styles._getCss();
+// const MiniListingCSS = styles._getCss();
 
 const MiniListing = ({data}) => {
 
   return (
     <div>
       {/* comment below out for client side rendering */}
-      <style>{MiniListingCSS}</style>
+      {/* <style>{MiniListingCSS}</style> */}
       <div className={styles.miniListing}>
         <div>
           <img  className={styles.image} src={data.imageUrl} />
